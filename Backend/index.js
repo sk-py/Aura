04344f1a7f9d0b151f40936a5a61ca7eb5ea0e5c
sk-py <77550660+sk-py@users.noreply.github.com>
2023-12-11 19:26:28 +0530

    index.js configured

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const dbConnection = require("./connection");
+require("dotenv").config();
+const PORT = process.env.PORT;
 //Routes imports
 const authRoutes = require("./Routes/Auth");
 const JobRoutes = require("./Routes/Jobs");
 const ExperienceRoutes = require("./Routes/Experience");
+const ApplicationRoutes = require("./Routes/JobApplications");
+
 //MiddleWare imports
 const authenticate = require("./controllers/jobValidator");
 
@@ -15,15 +19,17 @@ app.use(bodyParser.json());
 
 dbConnection();
 
-
 app.use("/api/auth", authRoutes);
-app.get("/",(req,res)=>{
-  return res.end("HII FROM SERVER")
-})
+app.use("/jobs", JobRoutes);
+app.use("/experience", ExperienceRoutes);
+app.use("/application", ApplicationRoutes);
+app.get("/", (req, res) => {
+  return res.end("HII FROM SERVER");
+});
 // app.use("/jobs", JobRoutes);
 // app.use("/experience", ExperienceRoutes);
 // app.use(authenticate);
 
-app.listen(9000, () => {
-  console.log("Server started on PORT 9000");
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
 });
